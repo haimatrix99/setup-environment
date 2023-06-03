ae8b8192f30735ecc1b861528710c818ecc82ed7
Le Trung Hai <letrunghai.92nl@gmail.com>
2023-06-03 15:50:49 +0700

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # setup-environment
+
+
+## Setup VIM Text Editor
+```bash
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+```
+
+## Setup One Dark Theme in VIM
+```bash
+cat > ~/.vim_runtime/my_configs.vim << EOF
+call plug#begin('~/.vim/plugged')
+    Plug 'joshdick/onedark.vim'
+call plug#end()
+EOF
+
+echo "syntax on\ncolorscheme onedark" >> ~/.vimrc
+```
--- setup.sh
+++ setup.sh
@@ -10,46 +10,19 @@ echo
 echo -e -e "\e[93mInstall Terraform and add to PATH...\e[0m"
 sudo snap install terraform --classic
 
-# Add Terraform to PATH
-echo"Paths to DevOps Tools" >> ~/.zshrc
-echo'export PATH="$PATH:/usr/local/bin/terraform"' >> ~/.zshrc
-source ~/.zshrc
-echo
 
 # Install Ansible
 echo -e "\e[93mInstall Ansible and add to PATH...\e[0m"
 sudo apt-get install -y ansible
 
-# Add Ansible to PATH
-echo 'export PATH="$PATH:/usr/bin/ansible"' >> ~/.zshrc
-source ~/.zshrc
-echo
 
 # Install Git
 sudo apt-get install -y git
 
-# Add Git to Path
-echo 'export PATH="$PATH:/usr/bin/git"' >> ~/.zshrc
-source ~/.zshrc
-echo
-
-# Install Docker
-#sudo snap install docker
-
-# Add Docker to PATH
-#echo 'export PATH="$PATH:/usr/bin/docker"' >> ~/.zshrc
-#source ~/.zshrc
-#echo
 
 # Install Kubernetes
 echo -e "\e[93mInstall Kubernetes and add to PATH...\e[0m"
 snap install kubectl
-
-
-# Add Kubernetes to PATH
-echo 'export PATH="$PATH:/usr/local/bin/kubectl"' >> ~/.zshrc
-source ~/.zshrc
-echo
 
 # Install Helm
 echo -e "\e[93mInstall Helm and add to PATH...\e[0m"
@@ -69,11 +42,6 @@ echo 'export PATH="$PATH:/usr/local/bin/aws"' >> ~/.zshrc
 source ~/.zshrc
 echo
 
-# Add kubie to PATH
-echo 'export PATH="$PATH:~/.kubie"' >> ~/.zshrc
-source ~/.zshrc
-echo
-
 # Add kubectl aliases to zshrc file
 ## Generate a secure password and copy it to clipboard
 echo -e "\e[93mAdd kubectl aliases to zshrc file...\e[0m"
@@ -90,8 +58,6 @@ alias kgs="k get service"
 alias kgpv="k get pv"
 alias kgpvc="k get pvc"
 alias kd="k describe"
-alias kns="kubie ns"
-alias kcx="kubie ctx"
 alias pod-check="kgp -A -o wide | grep -v Running | grep -v Comp"
 alias pod-count="kgp -A | wc -l"
 
@@ -109,17 +75,8 @@ alias gcm="git commit -m"
 alias gp="git push"
 alias gpt="git push --tags"
 
-## Ansible
-alias ansible="/usr/local/opt/ansible@2.8/bin/ansible"
-alias ansible-playbook="/usr/local/opt/ansible@2.8/bin/ansible-playbook"
-alias ansible2="/usr/local/opt/ansible@2.9/bin/ansible"
-alias ansible2-playbook="/usr/local/opt/ansible@2.9/bin/ansible-playbook"
-alias ansible4="/usr/local/opt/ansible/bin/ansible"
-alias ansible4-galaxy="/usr/local/opt/ansible/bin/ansible-galaxy"
-alias ansible4-playbook="/usr/local/opt/ansible/bin/ansible-playbook"
-
 ## Helm
-alias helm="/usr/local/opt/helm/bin/helm"
+alias helm="/usr/local/bin/helm"
 
 ## Zsh Auto Complete
 autoload -U up-line-or-beginning-search
